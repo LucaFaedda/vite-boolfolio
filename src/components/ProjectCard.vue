0d5e8ce0870f3a95fde9bb55367de029fd076879
LucaFaedda <lucakarl91@gmail.com>
2023-03-09 17:11:38 +0100

    aggiustato image

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,18 +1,14 @@
 <script>
 export default {
     props:{
-        card:Object, // per richiamarlo per stampare la visualizzazzione bisogna usare card o comunque il nome dato nella props
-    },
-    data(){
-      return{
-        baseUrl: 'http://127.0.0.1:8000/',
-      }
+        card:Object,
+        url:String // per richiamarlo per stampare la visualizzazzione bisogna usare card o comunque il nome dato nella props
     },
 }
 </script>
 <template >
     <div class="card mx-5 my-3" style="width: 18rem;">
-        <img :src="card.cover_image" class="card-img-top" :alt="card.title">
+        <img :src=" card.cover_image != null ? `${url}storage/${card.cover_image}` : 'https://picsum.photos/200/130'" :alt="card.title">
         <div class="card-body">
             <h5 class="card-title">{{card.title}}</h5>
             <p class="card-text">{{card.descrizione}}</p>
@@ -22,5 +18,5 @@ export default {
 </template>
 
 <style lang="scss">
-    
+
 </style>
