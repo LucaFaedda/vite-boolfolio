a79fb8f6d21b3de21ab2429f17009d9a603d3607
LucaFaedda <lucakarl91@gmail.com>
2023-03-09 15:54:52 +0100

    millstone 4 done

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,28 @@
 <script>
+
+import axios from 'axios';
   export default {
-          
+    data(){
+      return{
+        projects: [],
+        baseUrl: 'http://127.0.0.1:8000/',
+        loading: true
+      }
+    },
+    methods:{
+      getProjects(){
+        axios.get(`${this.baseUrl}api/projects`).then((response) => {
+          if(response.data.success){
+            console.log(response.data)
+            this.projects = response.data.results;
+            this.loading = false;
+          }
+        })
+      }
+    },
+    created(){
+      this.getProjects();
+    }
   }
 </script>
 <template lang="">
