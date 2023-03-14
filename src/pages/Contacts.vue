9345e280f59270c425a892b2df5aa7840852159d
LucaFaedda <lucakarl91@gmail.com>
2023-03-14 10:02:29 +0100

    aggiunto errori e button invio in corso

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -12,6 +12,8 @@ export default {
             phone: '',
             message: '',
             success: false,
+            loading: false,
+            errors: {}
 
         }
     },
@@ -25,9 +27,15 @@ export default {
                 message: this.message
             }
 
+            this.loading = true;
+            this.errors= {}
+            
+
             axios.post(`${this.store.baseUrl}api/contacts`, data).then((response) =>{
                 if(!response.data.success){
+                    console.log(response.data.errors)
                     this.errors = response.data.errors
+                    this.loading = false
                 }
                 else{
                     this.nome= '',
@@ -35,7 +43,9 @@ export default {
                     this.email='',
                     this.phone= '',
                     this.message= '' 
-                    this.success= true
+                    this.success= true,
+                    this.loading = false
+                    
                 }
             })
         }
@@ -72,28 +82,43 @@ export default {
                         <div class="col-6">
                             <label class="control-label fw-bold my-2" for="nome">Nome</label>
                             <input type="text" class="form-control" name="nome" id="nome" v-model="nome" placeholder="Inserisci il nome">
+                            <div  v-for="(error, index) in errors.nome" :key="`message-error-${index}`" class="alert alert-danger my-2">     
+                                <p  class="fw-bold">{{error}}</p>
+                            </div>
                         </div>
                         <div class="col-6">
                             <label class="control-label fw-bold my-2" for="cognome">Cognome</label>
                             <input type="text" class="form-control" name="cognome" id="cognome" v-model="cognome" placeholder="Inserisci il cognome">
+                           <div  v-for="(error, index) in errors.cognome" :key="`message-error-${index}`" class="alert alert-danger my-2">     
+                                <p  class="fw-bold">{{error}}</p>
+                            </div>
                         </div>
                     </div>
                     <div class="col-12">
                         <div class="col-6">
                             <label class="control-label fw-bold my-2" for="mail">Email</label>
                             <input type="mail" class="form-control" name="mail" id="mail" v-model="email" placeholder="Inserisci l'email">
+                            <div  v-for="(error, index) in errors.email" :key="`message-error-${index}`" class="alert alert-danger my-2">     
+                                <p  class="fw-bold">{{error}}</p>
+                            </div>
                         </div>
                         <div class="col-6">
                             <label class="control-label fw-bold my-2" for="phone">Telefono</label>
                             <input type="phone" class="form-control" name="phone" id="phone" v-model="phone" placeholder="Inserisci il Numero di telefono">
+                            <div  v-for="(error, index) in errors.phone" :key="`message-error-${index}`" class="alert alert-danger my-2">     
+                                <p  class="fw-bold">{{error}}</p>
+                            </div>
                         </div>
                     </div>
                     <div class="col-12">
                           <label for="message" class="form-label fw-bold my-2">Scrivi il tuo messaggio</label>
                           <textarea class="form-control" name="message" id="message" rows="3" placeholder="Inserisci un messaggio" v-model="message"></textarea>
+                          <div  v-for="(error, index) in errors.message" :key="`message-error-${index}`" class="alert alert-danger my-2">     
+                                <p  class="fw-bold">{{error}}</p>
+                            </div>
                     </div>
                     <div class="col-4 mb-5">
-                        <button type="submit" class="btn-personale mt-4">Invia</button>
+                        <button type="submit" class="btn-personale mt-4" :disable="loading">{{loading ? 'Invio in corso...' : 'invio'}}</button>
                     </div>
                 </form>
             </div>
@@ -109,7 +134,7 @@ export default {
         border-radius: 45px;
         transition: all 0.3s;
         cursor: pointer;
-        font-size: 1.2rem;
+        // font-size: 1.2rem;
         background-color: #315cfd;
         border: 3px solid #315cfd;
         
@@ -117,7 +142,7 @@ export default {
             // border: #315cfd;
             background: #fff;
             color:#315cfd;
-            font-size: 1.5rem;
+            // font-size: 1.5rem;
         }
     }
     
